c045a9e6818d2a11b1bf7a369134f2b2049efa06
israel <easy4sng@gmail.com>
2023-09-30 14:31:23 +0800

    add options page

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import {
   Card,
@@ -7,16 +7,51 @@ import {
   Grid,
   Box,
   TextField,
+  Button,
 } from "@mui/material";
+import {
+  LocalStorageOptions,
+  getStoredOptions,
+  setStoredOptions,
+} from "../utils/storage";
 import "fontsource-roboto";
 import "./options.css";
 
+type FormState = "ready" | "save";
 const App: React.FC<{}> = () => {
+  const [options, setOptions] = useState<LocalStorageOptions | null>(null);
+  const [formState, setFormState] = useState<FormState>("ready");
+  const isSavingData = "save" === formState;
+
+  const handleHomeCityChange = (homeCity: string) => {
+    setOptions({
+      ...options,
+      homeCity,
+    });
+  };
+
+  const handleSaveButtonClick = () => {
+    setFormState("save");
+    setStoredOptions(options).then(() => {
+      setTimeout(() => {
+        setFormState("ready");
+      }, 1000);
+    });
+  };
+
+  useEffect(() => {
+    getStoredOptions().then((storedOptions) => {
+      setOptions(storedOptions);
+    });
+  }, []);
+  if (!options) {
+    return null;
+  }
   return (
     <Box mx="10%" my="2%">
       <Card>
         <CardContent>
-          <Grid container direction="column">
+          <Grid container direction="column" spacing={4}>
             <Grid item>
               <Typography variant="h4" component="h4">
                 Weather Extension Options
@@ -26,7 +61,23 @@ const App: React.FC<{}> = () => {
               <Typography variant="body1" component="p">
                 Home city name
               </Typography>
-              <TextField fullWidth placeholder="Enter a home city name" />
+              <TextField
+                fullWidth
+                placeholder="Enter a home city name"
+                value={options.homeCity}
+                onChange={(e) => handleHomeCityChange(e.target.value)}
+                disabled={isSavingData}
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSaveButtonClick}
+                disabled={isSavingData}
+              >
+                {isSavingData ? "Saving..." : "Save"}
+              </Button>
             </Grid>
           </Grid>
         </CardContent>
